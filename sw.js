5740e4f338966af8730eca284fbc42d754448950
betokasa1982-del <betokasa1982@gmail.com>
2026-06-08 12:53:27 +0200

    Map improve plus functionalities

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // GPX Navigator Pro — Service Worker
 // Versão do cache: incrementar ao atualizar os arquivos
-const CACHE = "gpx-nav-v1";
+const CACHE = "gpx-nav-v4";
 const ASSETS = [
   "./",
   "./index.html",
